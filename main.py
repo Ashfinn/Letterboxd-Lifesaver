7a115632e2133aca76cce4a4c768423c61d96f2f
Ashfin <obidur.shawal@gmail.com>
2023-12-25 11:49:54 +0600

    modified:   main.py

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ from bs4 import BeautifulSoup
 import random
 
 data = []
+while True:
 
-response = requests.get('https://letterboxd.com/ashfin/watchlist/')
-soup = BeautifulSoup(response.text, 'html.parser')
+    response = requests.get('https://letterboxd.com/ashfin/watchlist/')
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-for e in soup.select('li.poster-container'):
-    img = e.find('img')
-    if img and img.has_attr('alt'):
-        data.append(img['alt'])
+    for e in soup.select('li.poster-container'):
+        img = e.find('img')
+        if img and img.has_attr('alt'):
+            data.append(img['alt'])
 
-num = random.randint(0, len(data))
-print(data[num])
+    num = random.randint(0, len(data))
+    print(data[num])
+    exit_value = input("type 1 to exit:   ")
+    if exit_value == '1':
+        break
